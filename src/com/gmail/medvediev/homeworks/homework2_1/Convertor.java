f79e0f3baf1319041f1da64d52b7d5eeea1b2568
highterban <nikol.med96@gmail.com>
2025-06-29 14:36:40 +0300

    Add converting miles to kilometres

--- src/com/gmail/medvediev/homeworks/homework2_1/Convertor.java
+++ src/com/gmail/medvediev/homeworks/homework2_1/Convertor.java
@@ -1,8 +1,17 @@
 package com.gmail.medvediev.homeworks.homework2_1;
 
 public class Convertor {
+    public static final double CONV_K = 1.60934;
+
     public static void main(String[] args) {
         System.out.println("This app is using for measures converting");
+        double miles = 10;
+        double kilometres = 12;
+        kilometres= convMilesToKilometres(miles);
+        System.out.println(kilometres + " kilometres");
+    }
+    private static double convMilesToKilometres(double miles) {
+        return miles * CONV_K;
     }
 
 }
