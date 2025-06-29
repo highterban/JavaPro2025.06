19f1529c10a0852060e41b8acbfb2227ca4fb8bf
highterban <nikol.med96@gmail.com>
2025-06-29 14:46:26 +0300

    Make code better and add function of converting kilometres to miles

--- src/com/gmail/medvediev/homeworks/homework2_1/Convertor.java
+++ src/com/gmail/medvediev/homeworks/homework2_1/Convertor.java
@@ -6,12 +6,18 @@ public class Convertor {
     public static void main(String[] args) {
         System.out.println("This app is using for measures converting");
         double miles = 10;
-        double kilometres = 12;
-        kilometres= convMilesToKilometres(miles);
-        System.out.println(kilometres + " kilometres");
+        double kilometres = convMilesToKilometres(miles);
+        System.out.printf("%.2f kilometres %n", kilometres);
+        kilometres = 20;
+        miles = convKilometresToMiles(kilometres);
+        System.out.printf("%.2f miles %n", miles);
     }
+
     private static double convMilesToKilometres(double miles) {
         return miles * CONV_K;
     }
 
+    private static double convKilometresToMiles(double kilometres) {
+        return kilometres / CONV_K;
+    }
 }
