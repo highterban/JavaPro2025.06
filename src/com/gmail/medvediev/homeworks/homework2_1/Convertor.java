43c22bae43228904fe64d2e9bfc3a3d6e61304d4
highterban <nikol.med96@gmail.com>
2025-06-29 14:23:37 +0300

    Create app for measures converting

---
+++ src/com/gmail/medvediev/homeworks/homework2_1/Convertor.java
@@ -0,0 +1,8 @@
+package com.gmail.medvediev.homeworks.homework2_1;
+
+public class Convertor {
+    public static void main(String[] args) {
+        System.out.println("This app is using for measures converting");
+    }
+
+}
