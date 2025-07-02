78183016d338c69780d9977224c15e450373d566
highterban <nikol.med96@gmail.com>
2025-07-02 14:22:46 +0300

    Create app for converting temperature

---
+++ src/com/gmail/medvediev/homeworks/homework3_1/TemperatureConvertor.java
@@ -0,0 +1,7 @@
+package com.gmail.medvediev.homeworks.homework3_1;
+
+public class TemperatureConvertor {
+    public static void main(String[] args) {
+        System.out.println("This app is using for converting temperature");
+    }
+}
