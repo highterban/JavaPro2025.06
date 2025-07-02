1bcdf4f39288ef4f618dfcaa89196e025c25ee75
highterban <nikol.med96@gmail.com>
2025-07-02 14:37:25 +0300

    Create function of converting fahrenheits to Celsius

--- src/com/gmail/medvediev/homeworks/homework3_1/TemperatureConvertor.java
+++ src/com/gmail/medvediev/homeworks/homework3_1/TemperatureConvertor.java
@@ -3,5 +3,13 @@ package com.gmail.medvediev.homeworks.homework3_1;
 public class TemperatureConvertor {
     public static void main(String[] args) {
         System.out.println("This app is using for converting temperature");
+        double fahrenheit = 100;
+        double celsius;
+        celsius = convFahrenheitToCelsius(fahrenheit);
+        System.out.printf("%.2f fahrenheit to celsius: %.2f %n", fahrenheit, celsius);
+    }
+
+    public static double convFahrenheitToCelsius(double fahrenheit) {
+        return (fahrenheit - 32) * 5 / 9;
     }
 }
